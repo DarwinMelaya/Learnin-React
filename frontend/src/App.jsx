15a20762b41b26fb6da972fab92503c03efe75e4
DarwinMelaya <116789003+DarwinMelaya@users.noreply.github.com>
2024-12-09 04:58:53 +0800

    Destructuring Props

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,7 @@ import Hello from "./components/Hello";
 function App() {
   return (
     <div className="App">
-      <Hello name="Darwin" message="Hi there!" />
+      <Hello name="Darwin" message="Hi there!" emoji="👋" />
     </div>
   );
 }
