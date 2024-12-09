68dd671cc0707eabf3b8a43b63563c408db6f440
DarwinMelaya <116789003+DarwinMelaya@users.noreply.github.com>
2024-12-09 23:55:48 +0800

    Using Ternary Operators

--- frontend/src/components/CondtionalComponent.jsx
+++ frontend/src/components/CondtionalComponent.jsx
@@ -2,12 +2,6 @@ import Welcome from "./Welcome";
 import Code from "./Code";
 
 export default function CondtionalComponent() {
-  let message;
   const display = true;
-  if (display) {
-    message = <h1>This is message 1</h1>;
-  } else {
-    message = <h1>This is message 2</h1>;
-  }
-  return message;
+  return display ? <Welcome /> : <Code />;
 }
